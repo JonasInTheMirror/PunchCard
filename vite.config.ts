1be8e93a9fb565c1e2e89ec5faffc44e21c45ab8
Jonas Ng <jonas.n@ikigai.team>
2026-03-12 10:30:04 +0800

    feat: offline pwa caching for 0ms load times

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        name: 'Punch Card & Voice Assistant',
+        short_name: 'Punch',
+        description: 'Super fast punch card tracker',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
