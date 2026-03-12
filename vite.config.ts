3ed579397f5dbae87ff9da331a059bdeb0347e01
JonasInTheMirror <jonasng9898@gmail.com>
2026-03-12 13:30:59 +0800

    Revert "Add Cloudflare Workers configuration"

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,26 @@ import path from 'path'
 
 import { VitePWA } from 'vite-plugin-pwa'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: 'auto',
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-    },
-    manifest: {
-      name: 'Punch Card & Voice Assistant',
-      short_name: 'Punch',
-      description: 'Super fast punch card tracker',
-      theme_color: '#000000',
-      background_color: '#000000',
-      display: 'standalone',
-    }
-  }), cloudflare()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        name: 'Punch Card & Voice Assistant',
+        short_name: 'Punch',
+        description: 'Super fast punch card tracker',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
